75ddfa3471ca9ebd48737a028acfb318c91a009c
kyn <hwarangmoon@gmail.com>
2015-06-08 22:49:02 +0900

    Revert "Deburg"
    This reverts commit 0481db20bf6ebce66fe42c09fd9fb0bfae120617.

--- src/AppResourceId.cpp
+++ src/AppResourceId.cpp
@@ -1,5 +1,8 @@
 #include "AppResourceId.h"
 
+const wchar_t* IDC_BUTTON1_Optimization = L"IDC_BUTTON1_Optimization";
+const wchar_t* IDC_BUTTON1_Performance = L"IDC_BUTTON1_Performance";
+const wchar_t* IDC_BUTTON2_PowerSaving = L"IDC_BUTTON2_PowerSaving";
 const wchar_t* IDC_BUTTON_Optimization = L"IDC_BUTTON_Optimization";
 const wchar_t* IDC_BUTTON_PowerSaving = L"IDC_BUTTON_PowerSaving";
 const wchar_t* IDL_FORM = L"IDL_FORM";
