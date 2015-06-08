0481db20bf6ebce66fe42c09fd9fb0bfae120617
kyn <hwarangmoon@gmail.com>
2015-06-08 22:48:28 +0900

    Deburg
    File Open/File Write

--- inc/AppResourceId.h
+++ inc/AppResourceId.h
@@ -1,9 +1,6 @@
 #ifndef _APP_RESOURCE_ID_H_
 #define _APP_RESOURCE_ID_H_
 
-extern const wchar_t* IDC_BUTTON1_Optimization;
-extern const wchar_t* IDC_BUTTON1_Performance;
-extern const wchar_t* IDC_BUTTON2_PowerSaving;
 extern const wchar_t* IDC_BUTTON_Optimization;
 extern const wchar_t* IDC_BUTTON_PowerSaving;
 extern const wchar_t* IDL_FORM;
